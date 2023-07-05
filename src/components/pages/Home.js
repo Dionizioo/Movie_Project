9157c3065b93d6d328d83bd4731794a0e6fe49fe
Dionizio <88460475+Dionizioo@users.noreply.github.com>
2023-07-05 14:36:32 -0300

    Aula 20
    Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,20 @@
+import styles from "./Home.module.css"
+import savings from '../../img/savings.svg'
+import LinkButton from "../layout/LinkButton"
+
 function Home(){
-    return <h1>Home</h1>
+    return(
+        <section className={styles.home_container}>
+            <h1>Bem-Vindo ao <span>Cost</span></h1>
+            <p>Começe a gerenciasr os seus projetos agora mesmo!</p>
+            <LinkButton to="/newproject" text="Criar Projeto"/>
+            <img src={savings} alt="Cost" />
+
+        </section>
+    )
 }
 export default Home
+
+
+//section trablar em sessãoes
+// coloca um destaque
