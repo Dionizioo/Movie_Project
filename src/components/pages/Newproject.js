9d68d1dffa899614f76037581bb7abfb8212cca2
Dionizio <88460475+Dionizioo@users.noreply.github.com>
2023-07-06 14:29:32 -0300

    Aula 24
    Inserindo Dados no Banco

--- src/components/pages/Newproject.js
+++ src/components/pages/Newproject.js
@@ -1,14 +1,39 @@
-import ProjectForm from "../project/ProjectForm"
+import { useHistory } from 'react-router-dom'
 
-import styles from "./Newproject.module.css"
+import ProjectForm from '../project/ProjectForm'
 
-function NewProject(){
-    return(
-        <div className={styles.newproject_container}>
-            <h1>Criar Projeto</h1>
-            <p>Crie seu projeto para depois adcionar os serviços</p>
-            <ProjectForm btnText="Criar Projeto" />
-        </div>
-    )
+import styles from './Newproject.module.css'
+
+//Inserindo dados no banco
+function NewProject() {
+    //Permita direcionar o usuario ,para outra pagina
+  const history = useHistory()
+
+  function createPost(project) {
+    // initialize cost and services
+    project.cost = 0
+    project.services = []
+
+    fetch('http://localhost:5000/projects', {
+      method: 'POST', //envio
+      headers: {
+        'Content-Type': 'application/json', 
+      },
+      body: JSON.stringify(project), //mandar dados por POST
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        history.push('/projects', { message: 'Projeto criado com sucesso!' })
+      })
+  }
+
+  return (
+    <div className={styles.newproject_container}>
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
+    </div>
+  )
 }
+
 export default NewProject
