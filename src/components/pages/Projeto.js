3fbaf32560fbd21a8484e550234c9911a0743001
Dionizio <88460475+Dionizioo@users.noreply.github.com>
2023-07-07 11:23:35 -0300

    Aula 26
    Criando Dashboard

--- src/components/pages/Projeto.js
+++ src/components/pages/Projeto.js
@@ -1,5 +1,8 @@
 import Message from "../layout/Message"
-import styles from "../layout/Message.module.css"
+import Container from "../layout/Container"
+import LinkButton from "../layout/LinkButton"
+
+import styles from './Projeto.module.css'
 
 import{useLocation} from 'react-router-dom'
 
@@ -14,9 +17,15 @@ function Projeto(){
     }
 
     return(
-        <div>
-            <h1>Meu Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meu Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto"/>
+            </div>
             {message && <Message type="success" msg={message} />}
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
         </div>
     ) 
 }
